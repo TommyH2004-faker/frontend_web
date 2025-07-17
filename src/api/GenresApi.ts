d3481618d1d3d81e38f4f054b29a6a095b35c24d
Tommy <hiept81331@gmail.com>
2025-07-17 22:38:39 +0700

    update file sau khi sua xong upload anh va add plastic moi

--- src/api/GenresApi.ts
+++ src/api/GenresApi.ts
@@ -17,15 +17,15 @@ async function getGenre(endpoint: string): Promise<GenresApiProps> {
     return { genreList: genreList, genre: response.genre };
 }
 export async function  getGenreById(idPlastic: number): Promise<GenresApiProps> {
-    const endpoint = `https://backend-webtommysf.onrender.com/plastic-items/${idPlastic}/listGenres`;
+    const endpoint = `https://backend-website-j4f1.onrender.com/plastic-items/${idPlastic}/listGenres`;
     return getGenre(endpoint);
 }
 export async function getAllGenres(): Promise<GenresApiProps> {
-    const endpoint = "https://backend-webtommysf.onrender.com/genres?sort=idGenre";
+    const endpoint = "https://backend-website-j4f1.onrender.com/genres?sort=idGenre";
     return getGenre(endpoint);
 }
 export async function get1Genre(idGenre: number): Promise<GenresApiProps> {
-    const endpoint = `https://backend-webtommysf.onrender.com/genres/${idGenre}`;
+    const endpoint = `https://backend-website-j4f1.onrender.com/genres/${idGenre}`;
     const response = await my_request(endpoint);
     return { genre: response, genreList: response };
 }
