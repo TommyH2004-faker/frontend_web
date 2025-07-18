b9cab6cf9d8eff2fee967a2321f2f5eb3234aa0a
Tommy <hiept81331@gmail.com>
2025-07-18 22:02:11 +0700

    update file sau khi sua xong upload anh va add plastic moi

--- src/layouts/header-footer/Footer.tsx
+++ src/layouts/header-footer/Footer.tsx
@@ -2,65 +2,50 @@ import React from "react";
 
 function Footer() {
     return (
-        <div className="container">
-            <footer className="py-5">
-                <div className="row">
-                    <div className="col-6 col-md-2 mb-3">
-                        <h5>Section</h5>
-                        <ul className="nav flex-column">
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
+        <footer className="footer-section">
+            <div className="footer-container">
+                <div className="footer-brand">
+                    <img src={process.env.PUBLIC_URL + '/images/Plastics/Logo.png'} alt="Logo" className="footer-logo" />
+                    <p className="footer-slogan">Nâng tầm cuộc sống với nhựa chất lượng!</p>
+                </div>
+                <div className="footer-columns">
+                    <div className="footer-col">
+                        <h4>Về chúng tôi</h4>
+                        <ul>
+                            <li><a href="#">Giới thiệu</a></li>
+                            <li><a href="#">Sản phẩm</a></li>
+                            <li><a href="#">Tin tức</a></li>
+                            <li><a href="#">Liên hệ</a></li>
                         </ul>
                     </div>
-
-                    <div className="col-6 col-md-2 mb-3">
-                        <h5>Section</h5>
-                        <ul className="nav flex-column">
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
+                    <div className="footer-col">
+                        <h4>Hỗ trợ khách hàng</h4>
+                        <ul>
+                            <li><a href="#">Chính sách bảo hành</a></li>
+                            <li><a href="#">Chính sách đổi trả</a></li>
+                            <li><a href="#">Hướng dẫn mua hàng</a></li>
+                            <li><a href="#">Câu hỏi thường gặp</a></li>
                         </ul>
                     </div>
-
-                    <div className="col-6 col-md-2 mb-3">
-                        <h5>Section</h5>
-                        <ul className="nav flex-column">
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">FAQs</a></li>
-                            <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">About</a></li>
-                        </ul>
-                    </div>
-
-                    <div className="col-md-5 offset-md-1 mb-3">
-                        <form>
-                            <h5>Subscribe to our newsletter</h5>
-                            <p>Monthly digest of what's new and exciting from us.</p>
-                            <div className="d-flex flex-column flex-sm-row w-100 gap-2">
-                                <label  className="visually-hidden">Email address</label>
-                                <input id="newsletter1" type="text" className="form-control" placeholder="Email address"/>
-                                <button className="btn btn-primary" type="button">Subscribe</button>
-                            </div>
-                        </form>
+                    <div className="footer-col">
+                        <h4>Kết nối</h4>
+                        <div className="footer-socials">
+                            <a href="#" aria-label="Facebook"><i className="fab fa-facebook-f"></i></a>
+                            <a href="#" aria-label="Instagram"><i className="fab fa-instagram"></i></a>
+                            <a href="#" aria-label="Twitter"><i className="fab fa-twitter"></i></a>
+                            <a href="#" aria-label="Youtube"><i className="fab fa-youtube"></i></a>
+                        </div>
+                        <div className="footer-contact">
+                            <p><i className="fas fa-phone-alt"></i> 0123 456 789</p>
+                            <p><i className="fas fa-envelope"></i> info@plasticshop.vn</p>
+                        </div>
                     </div>
                 </div>
-
-                <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-                    <p>&copy; 2023 Company, Inc. All rights reserved.</p>
-                    <ul className="list-unstyled d-flex">
-                        <li className="ms-3"><a className="link-body-emphasis" href="#"> <i className="fas fa-tweeter"></i></a></li>
-                        <li className="ms-3"><a className="link-body-emphasis" href="#"> <i className="fas fa-instagram"></i></a></li>
-                        <li className="ms-3"><a className="link-body-emphasis" href="#"> <i className="fas fa-facebook"></i></a></li>
-                    </ul>
-                </div>
-            </footer>
-        </div>
+            </div>
+            <div className="footer-bottom">
+                <p>© {new Date().getFullYear()} Plastic Shop. All rights reserved.</p>
+            </div>
+        </footer>
     );
 }
 
