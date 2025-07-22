ed0c2b71ed758fbc26d01660ac62eaf2e2a001e0
Tommy <hiept81331@gmail.com>
2025-07-22 09:11:49 +0700

    config lai cho che do xem dien thoai

--- src/layouts/header-footer/Navbar.tsx
+++ src/layouts/header-footer/Navbar.tsx
@@ -1,3 +1,4 @@
+/*
 import React, {useEffect} from "react";
 import { useState } from "react";
 import {Link, NavLink, useLocation, useNavigate} from "react-router-dom";
@@ -102,7 +103,7 @@ function Navbar({tuKhoaTimKiem,setTuKhoaTimKiem}:NavbarProps) {
                     </ul>
                 </div>
 
-                {/* Tìm kiếm */}
+                {/!* Tìm kiếm *!/}
                 <div className="d-flex">
                     <input className="form-control me-2" type="search" placeholder="Tìm kiếm" aria-label="Search"
                            onChange={onsearchInputChange} value={tuKhoaTamThoi}/>
@@ -111,7 +112,7 @@ function Navbar({tuKhoaTimKiem,setTuKhoaTimKiem}:NavbarProps) {
                     </button>
                 </div>
 
-                {/* Biểu tượng giỏ hàng */}
+                {/!* Biểu tượng giỏ hàng *!/}
                 <ul className="navbar-nav me-1">
                     <li className="nav-item">
                         <Link className="nav-link" to="/cart">
@@ -121,7 +122,7 @@ function Navbar({tuKhoaTimKiem,setTuKhoaTimKiem}:NavbarProps) {
                     </li>
                 </ul>
 
-                {/* Biểu tượng đăng nhập */}
+                {/!* Biểu tượng đăng nhập *!/}
                 {!isToken() && (
                     <div>
                         <Link to={"/dangnhap"}>
@@ -134,7 +135,7 @@ function Navbar({tuKhoaTimKiem,setTuKhoaTimKiem}:NavbarProps) {
                 )}
                 {isToken() && (
                     <>
-                        {/* <!-- Notifications --> */}
+                        {/!* <!-- Notifications --> *!/}
                         <div className='dropdown'>
                             <a
                                 className='text-reset me-3 dropdown-toggle hidden-arrow'
@@ -170,7 +171,7 @@ function Navbar({tuKhoaTimKiem,setTuKhoaTimKiem}:NavbarProps) {
                                 </li>
                             </ul>
                         </div>
-                        {/* <!-- Avatar --> */}
+                        {/!* <!-- Avatar --> *!/}
                         <div className='dropdown'>
                             <a
                                 className='dropdown-toggle d-flex align-items-center hidden-arrow'
@@ -232,6 +233,179 @@ function Navbar({tuKhoaTimKiem,setTuKhoaTimKiem}:NavbarProps) {
                         </div>
                     </>
                 )}
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;*/
+import React, {useEffect, useState} from "react";
+import {Link, NavLink, useLocation, useNavigate} from "react-router-dom";
+import {getAvatarByToken, getLastNameByToken, getRoleByToken, isToken, logout} from "../utils/JwtService";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import GenreModel from "../../models/GenreModel";
+import {getAllGenres} from "../../api/GenresApi";
+import {Search} from "react-bootstrap-icons";
+import {useAuth} from "../utils/AuthContext";
+import {useCartItem} from "../../models/CartItemContext";
+
+interface NavbarProps {
+    tuKhoaTimKiem: string;
+    setTuKhoaTimKiem: (tuKhoa: string) => void;
+}
+
+function Navbar({tuKhoaTimKiem, setTuKhoaTimKiem}: NavbarProps) {
+    const [tuKhoaTamThoi, setTuKhoaTamThoi] = useState('');
+    const {totalCart, setTotalCart, setCartList} = useCartItem();
+    const {setLoggedIn} = useAuth();
+    const [genreList, setGenreList] = useState<GenreModel[]>([]);
+    const navigate = useNavigate();
+    const [erroring, setErroring] = useState(null);
+    const location = useLocation();
+
+    const adminEndpoint = ["/admin"];
+    if (adminEndpoint.includes(location.pathname)) return null;
+
+    useEffect(() => {
+        getAllGenres().then((response) => {
+            setGenreList(response.genreList);
+        }).catch((error) => {
+            setErroring(error.message);
+        });
+    }, []);
+
+    const onsearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setTuKhoaTamThoi(event.target.value);
+    };
+
+    const handleSearch = () => {
+        setTuKhoaTimKiem(tuKhoaTamThoi);
+    };
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <img src="/images/Plastics/Logo.png" alt="Logo" style={{width: "50px", height: "50px"}}/>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <NavLink className="nav-link active" to="/">Trang chủ</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/about">Giới thiệu</NavLink>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <button
+                                className="nav-link dropdown-toggle btn btn-link"
+                                id="navbarDropdown"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                                type="button"
+                            >
+                                Sản Phẩm
+                            </button>
+                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                {genreList.map((genre, index) => (
+                                    <li key={index}>
+                                        <Link className="dropdown-item" to={`/search/${genre.idGenre}`}>
+                                            {genre.nameGenre}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/policy">Chính sách</Link>
+                        </li>
+                        {isToken() && (
+                            <li className="nav-item">
+                                <NavLink className="nav-link" to="/feedback">Feedback</NavLink>
+                            </li>
+                        )}
+                    </ul>
+
+                    {/* Tìm kiếm */}
+                    <div className="d-flex">
+                        <input className="form-control me-2" type="search" placeholder="Tìm kiếm"
+                               onChange={onsearchInputChange} value={tuKhoaTamThoi}/>
+                        <button className="btn btn-outline-success" onClick={handleSearch}>
+                            Search <Search/>
+                        </button>
+                    </div>
+
+                    {/* Giỏ hàng */}
+                    <ul className="navbar-nav me-1">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/cart">
+                                <i className="fas fa-shopping-cart"></i>
+                                <span className="badge bg-danger">{totalCart ? totalCart : ""}</span>
+                            </Link>
+                        </li>
+                    </ul>
+
+                    {/* Đăng nhập/Avatar */}
+                    {!isToken() ? (
+                        <div>
+                            <Link to="/dangnhap"><Button>Đăng nhập</Button></Link>
+                            <Link to="/dangKy"><Button>Đăng ký</Button></Link>
+                        </div>
+                    ) : (
+                        <>
+                            {/* Thông báo */}
+                            <div className="dropdown">
+                                <a className="text-reset me-3 dropdown-toggle hidden-arrow"
+                                   href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <i className="fas fa-bell"></i>
+                                    <span className="badge rounded-pill badge-notification bg-danger">1</span>
+                                </a>
+                                <ul className="dropdown-menu dropdown-menu-end">
+                                    <li><a className="dropdown-item" href="#">Tin tức</a></li>
+                                    <li><a className="dropdown-item" href="#">Thông báo mới</a></li>
+                                    <li><a className="dropdown-item" href="#">Tất cả thông báo</a></li>
+                                </ul>
+                            </div>
+
+                            {/* Avatar */}
+                            <div className="dropdown">
+                                <a className="dropdown-toggle d-flex align-items-center hidden-arrow"
+                                   href="#" id="navbarDropdownMenuAvatar" role="button"
+                                   data-bs-toggle="dropdown" aria-expanded="false">
+                                    <Avatar
+                                        style={{fontSize: "14px"}}
+                                        alt={getLastNameByToken()?.toUpperCase()}
+                                        src={getAvatarByToken()}
+                                        sx={{width: 30, height: 30}}
+                                    />
+                                </a>
+                                <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuAvatar">
+                                    <li><Link to="/profile" className="dropdown-item">Thông tin cá nhân</Link></li>
+                                    <li><Link to="/my-favorite-books" className="dropdown-item">Sản phẩm yêu thích</Link></li>
+                                    {getRoleByToken() === "ADMIN" && (
+                                        <li><Link to="/admin/dashboard" className="dropdown-item">Quản lý</Link></li>
+                                    )}
+                                    <li>
+                                        <a className="dropdown-item" style={{cursor: "pointer"}}
+                                           onClick={() => {
+                                               setTotalCart(0);
+                                               logout(navigate);
+                                               setLoggedIn(false);
+                                               setCartList([]);
+                                           }}>
+                                            Đăng xuất
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </>
+                    )}
+                </div>
             </div>
         </nav>
     );
